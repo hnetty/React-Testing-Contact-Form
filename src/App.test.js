be071504e7aa57f3487d6f5a44fe66132f716d6d
Harper Nettesheim <harpernettesheim@Harpers-MacBook-Pro.local>
2020-07-09 17:45:21 -0500

    MVP changes of the day

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,23 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test('renders text on app', () => {
+  const {getByText} = render(<App />);
+
+  const text = getByText(/message/i);
+
+  expect(text).toBeInTheDocument();
+
+});
+
+test('tests the length of the first name input', () => {
+
+  const {getByInput} = render(<App/>);
+
+  const input = 'bob'
+
+  expect(input).toHaveLength(3);
+  
+
+});
